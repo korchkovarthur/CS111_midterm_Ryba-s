a56cf3604aab3a286d0cdcd26a05833a18966c58
Arthur Korchkov <qaeng16@gmail.com>
2020-11-13 22:52:46 +0000

    double loop

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-#include <cstring>
+
 using namespace std;
 
 
 int main() {
   
   cout << "\n\nStart\n\n";
-  cout << "Please, enter a day of the week: " << endl;
-  string str;
-  cin >> str;
-  if ((str == "Thursday") || (str == "thursday") || (str == "tuesday") || (str == "Tuesday")) {
-    cout << "You have a class today. " << endl;
-  }
+ int n;
+ cout << "What is n? " << endl;
+ cin >> n;
+
+for (int r = 0; r < n; r++ ){
+  for (int c = 0; c < r; c++ ){
+    //if (c - r <= 0 ) {
+     cout << "*";
+   // }
+   // else {
+    //  cout << ".";
+    //}
+  
+  }  
+  cout << endl; 
+}
 
 
-  cout << "\n\nStart\n\n";
+for (int r = n; r > 0; r-- ){
+  for (int c = 0; c < r; c++ ){
+    //if (c - r <= 0 ) {
+     cout << "*";
+   // }
+   // else {
+   //   cout << ".";
+    //}
+  
+  }  
+  cout << endl; 
+
+  
+}
+  cout << "\n\nFinish\n\n";
   return 0;
 }
